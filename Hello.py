6e742e9a4ef51a6381e6931a815c7d0b1414aef9
Owen <owen.theis@kearsarge.org>
2024-09-11 11:14:00 -0400

    Setup

---
+++ Hello.py
@@ -0,0 +1,58 @@
+name = input("What's your name? ")
+greeting = "Nice to meet you"
+ex = "!"
+lee = "Lee"
+leechat = "Lee: "
+intr = "I wanna get to know you,"
+print (lee + ":" + " " + greeting + " " + name + ex)
+print (leechat + intr)
+
+food = input("What's your favorite food? ")
+if food == "microplastic":
+    print(leechat + "oh...")
+    print (leechat + "wait no" + ex * 4 + " " + name + " that's really dangerous" + ex)
+    print(leechat + "uhh anyways,")
+elif food == "Microplastic":
+    print(leechat + "oh...")
+    print (leechat + "wait no" + ex * 4 + " " + name + " that's really dangerous" + ex)
+    print(leechat + "uhh anyways,")
+else:
+    print(leechat + "Oh thats so neat, I really love " + food + " Too!")
+
+color = input("what's your favorite color " + name + "? " )
+if color == "orange":
+    print (leechat + "Ewww, I really dont like orange" + ex)
+elif color == "Orange":
+    print (leechat + "Ewww, I really dont like orange" + ex)
+elif color == "pink":
+    print (leechat + "omg " + color + " is SO cute" + ex)
+elif color == "Pink":
+    print (leechat + "omg " + color + " is SO cute" + ex)
+elif color == "red":
+    print (leechat + "No way" + ex + " Me Too" + ex)
+elif color == "Red":
+    print (leechat + "No way" + ex + " Me Too" + ex)
+else:
+    print (leechat + "Oh cool, My favorite colour is red" + ex)
+
+supow = input("Hey " + name + ", if you could have any superpower, what would you choose? ")
+if supow == "flight":
+    print (leechat + "haha how basic " + name + " " + ex)
+elif supow == "Flight":
+    print (leechat + "haha how basic " + name + " " + ex)
+elif supow == "teleportation":
+    print (leechat + "yeah, I would choose teleporting too.")
+elif supow == "Teleportation":
+    print (leechat + "yeah, I would choose teleporting too.")
+else:
+    print (leechat + "Thats such a creative power! ive never thought of " + supow + " before" + ex)
+print ("???:" + "Wait... why did I just forget my name!😟")
+
+leename = input("What's my name!? ")
+if leename == "lee":
+    print ("leechat: Oh yeah! I remember now, Thanks!")
+elif leename == "Lee":
+    print ("leechat: Oh yeah! I remember now, Thanks!")
+else:
+    print ("???:...No.. I dont think thats right " + name)
+print (leechat + "Wow, I know so much more about you now! Thanks so much for chatting with me, Bye!")
